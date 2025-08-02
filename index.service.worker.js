e78625049ce5277e25ba404f4a89b711568f3a49
Andrien <andrew@gogoberidze.ru>
2025-08-03 00:12:52 +0300

    Actually not a debug build

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1754168223|1558812980';
+const CACHE_VERSION = '1754169151|423638730';
 /** @type {string} */
 const CACHE_PREFIX = 'TVBuilder-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
