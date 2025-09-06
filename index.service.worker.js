741cc92318f6ae0f2493675766097428408d26d4
andre <andrew@gogoberidze.ru>
2025-09-06 21:26:26 +0300

    Add link to source

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1757180462|69077649';
+const CACHE_VERSION = '1757183117|30482385';
 /** @type {string} */
 const CACHE_PREFIX = 'TVBuilder-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
